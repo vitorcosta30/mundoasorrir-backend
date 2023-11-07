e3a48dba01af1c764d356c21fbe9792af94d173a
1201391 <1201391@isep.ipp.pt>
2023-11-07 19:35:50 +0000

    Update to file class

--- src/main/java/com/mundoasorrir/mundoasorrirbackend/Domain/File/File.java
+++ src/main/java/com/mundoasorrir/mundoasorrirbackend/Domain/File/File.java
@@ -27,4 +27,31 @@ public class File {
     }
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
 }
