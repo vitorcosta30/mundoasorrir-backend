097602e9ba5d202870c42bb07b2602bcb7fc0c60
1201391 <1201391@isep.ipp.pt>
2023-11-07 19:34:44 +0000

    File upload update

--- src/main/java/com/mundoasorrir/mundoasorrirbackend/Controllers/FileUploadController.java
+++ src/main/java/com/mundoasorrir/mundoasorrirbackend/Controllers/FileUploadController.java
@@ -1,31 +1,76 @@
 package com.mundoasorrir.mundoasorrirbackend.Controllers;
 
+import com.mundoasorrir.mundoasorrirbackend.Domain.File.File;
+import com.mundoasorrir.mundoasorrirbackend.Message.ResponseFile;
+import com.mundoasorrir.mundoasorrirbackend.Message.ResponseMessage;
 import com.mundoasorrir.mundoasorrirbackend.Services.FileUploadService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
+@CrossOrigin(origins = "${mundoasorrir.app.frontend}", maxAge = 3600, allowCredentials = "true")
 @RestController
-@RequestMapping("/files")
+@RequestMapping("/api/files")
 @RequiredArgsConstructor
 public class FileUploadController {
+    @Autowired
+
     private final FileUploadService fileUploadService;
 
-    @PostMapping(value = "/upload", consumes = "multipart/form-data")
-    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
-        //log.info("File name: {}", file.getOriginalFilename());
 
-        fileUploadService.saveFileInDatabase(file);
+    @PostMapping("/upload")
+    //@PreAuthorize("hasRole('MANAGER')")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+        String message = "";
+        try {
+            fileUploadService.store(file);
 
-        return "File uploaded successfully";
+            message = "Uploaded the file successfully: " + file.getOriginalFilename();
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+        } catch (Exception e) {
+            message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+        }
     }
 
+    @GetMapping("/files")
+    public ResponseEntity<List<ResponseFile>> getListFiles() {
+        List<ResponseFile> files = fileUploadService.getAllFiles().map(dbFile -> {
+            String fileDownloadUri = ServletUriComponentsBuilder
+                    .fromCurrentContextPath()
+                    .path("/files/")
+                    .path(dbFile.getId().toString())
+                    .toUriString();
+
+            return new ResponseFile(
+                    dbFile.getName(),
+                    fileDownloadUri,
+                    dbFile.getType(),
+                    dbFile.getData().length);
+        }).collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.OK).body(files);
+    }
+
+    @GetMapping("/files/{id}")
+    public ResponseEntity<byte[]> getFile(@PathVariable Long id) {
+        File fileDB = fileUploadService.getFile(id);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileDB.getName() + "\"")
+                .body(fileDB.getData());
+    }
 
 }
--- src/main/java/com/mundoasorrir/mundoasorrirbackend/Services/FileUploadService.java
+++ src/main/java/com/mundoasorrir/mundoasorrirbackend/Services/FileUploadService.java
@@ -3,22 +3,36 @@ package com.mundoasorrir.mundoasorrirbackend.Services;
 import com.mundoasorrir.mundoasorrirbackend.Domain.File.File;
 import com.mundoasorrir.mundoasorrirbackend.Repositories.FileRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.stream.Stream;
+
 //@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FileUploadService {
-
+    @Autowired
     private final FileRepository fileRepository;
 
-    public void saveFileInDatabase(MultipartFile file) throws IOException {
-        File newFile = new File(file.getOriginalFilename(), file.getContentType(), file.getBytes());
-        fileRepository.save(newFile);
+
+    public File store(MultipartFile file) throws IOException {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        File FileDB = new File(fileName, file.getContentType(), file.getBytes());
+
+        return fileRepository.save(FileDB);
     }
 
+    public File getFile(Long id) {
+        return fileRepository.findById(id).get();
+    }
+
+    public Stream<File> getAllFiles() {
+        return fileRepository.findAll().stream();
+    }
 
 
 }
