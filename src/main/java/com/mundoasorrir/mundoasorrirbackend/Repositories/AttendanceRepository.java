4eef2d94b58714c2c1c7513bc9cc410ba2c1671a
1201391 <1201391@isep.ipp.pt>
2023-11-24 12:28:42 +0000

    Create attendance repository

---
+++ src/main/java/com/mundoasorrir/mundoasorrirbackend/Repositories/AttendanceRepository.java
@@ -0,0 +1,22 @@
+package com.mundoasorrir.mundoasorrirbackend.Repositories;
+
+import com.mundoasorrir.mundoasorrirbackend.Domain.Attendance.Attendance;
+import com.mundoasorrir.mundoasorrirbackend.Domain.Attendance.PresenceStatus;
+import com.mundoasorrir.mundoasorrirbackend.Domain.Attendance.Present;
+import com.mundoasorrir.mundoasorrirbackend.Domain.Event.Event;
+import com.mundoasorrir.mundoasorrirbackend.Domain.User.SystemUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+
+    Optional<Attendance> findByDayAttendanceEquals(Date date);
+
+
+;
+
+}
