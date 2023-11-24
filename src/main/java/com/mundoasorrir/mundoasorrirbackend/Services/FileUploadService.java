5636b2f12c20cf4bd75cc82c6f1d544345bbc832
1201391 <1201391@isep.ipp.pt>
2023-11-24 12:27:35 +0000

    Update to file service

--- src/main/java/com/mundoasorrir/mundoasorrirbackend/Services/FileUploadService.java
+++ src/main/java/com/mundoasorrir/mundoasorrirbackend/Services/FileUploadService.java
@@ -1,6 +1,7 @@
 package com.mundoasorrir.mundoasorrirbackend.Services;
 
 import com.mundoasorrir.mundoasorrirbackend.Domain.File.File;
+import com.mundoasorrir.mundoasorrirbackend.Domain.User.SystemUser;
 import com.mundoasorrir.mundoasorrirbackend.Repositories.FileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.stream.Stream;
 
 //@Slf4j
@@ -19,19 +21,20 @@ public class FileUploadService {
     private final FileRepository fileRepository;
 
 
-    public File store(MultipartFile file) throws IOException {
+    public File store(MultipartFile file, List<SystemUser> users, SystemUser creator) throws IOException {
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-        File FileDB = new File(fileName, file.getContentType(), file.getBytes());
+        File fileDB = new File(fileName, file.getContentType(), file.getBytes(), users);
+        fileDB.setSharedBy(creator);
 
-        return fileRepository.save(FileDB);
+        return fileRepository.save(fileDB);
     }
 
     public File getFile(Long id) {
         return fileRepository.findById(id).get();
     }
 
-    public Stream<File> getAllFiles() {
-        return fileRepository.findAll().stream();
+    public Stream<File> getAllFiles(String userName) {
+        return fileRepository.findByUsersAllowed_Username(userName).stream();
     }
 
 
