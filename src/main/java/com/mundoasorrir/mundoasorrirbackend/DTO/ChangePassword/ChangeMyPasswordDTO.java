8f0a6b9c852ed2e1b14f4a253eb726e5fc96f0d0
1201391 <1201391@isep.ipp.pt>
2023-12-10 16:27:56 +0000

    Change my password dto

---
+++ src/main/java/com/mundoasorrir/mundoasorrirbackend/DTO/ChangePassword/ChangeMyPasswordDTO.java
@@ -0,0 +1,34 @@
+package com.mundoasorrir.mundoasorrirbackend.DTO.ChangePassword;
+
+public class ChangeMyPasswordDTO {
+
+    private String oldPassword;
+
+    private String newPassword;
+
+    private String reNewPassword;
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getReNewPassword() {
+        return reNewPassword;
+    }
+
+    public void setReNewPassword(String reNewPassword) {
+        this.reNewPassword = reNewPassword;
+    }
+}
