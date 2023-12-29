eaca4dc0720806ec7cb29d2700da802267966329
1201391 <1201391@isep.ipp.pt>
2023-12-29 21:21:36 +0000

    Removing comments

--- src/main/java/com/mundoasorrir/mundoasorrirbackend/MundoasorrirBackendApplication.java
+++ src/main/java/com/mundoasorrir/mundoasorrirbackend/MundoasorrirBackendApplication.java
@@ -8,15 +8,4 @@ public class MundoasorrirBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(MundoasorrirBackendApplication.class, args);
     }
-/*
-    @RequestMapping("/user")
-    public Principal user(Principal user) {
-        return user;
-    }*/
-/*
-    @RequestMapping("/token")
-    public Map<String,String> token(HttpSession session) {
-        return Collections.singletonMap("token", session.getId());
-    }*/
-
 }
