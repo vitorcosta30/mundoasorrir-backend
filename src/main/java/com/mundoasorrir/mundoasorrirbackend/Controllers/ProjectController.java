d2b00850a59ce8e45545297ef89e06bf4b9a67e9
1201391 <1201391@isep.ipp.pt>
2023-12-30 20:50:29 +0000

    Updating project controller

--- src/main/java/com/mundoasorrir/mundoasorrirbackend/Controllers/ProjectController.java
+++ src/main/java/com/mundoasorrir/mundoasorrirbackend/Controllers/ProjectController.java
@@ -7,7 +7,9 @@ import com.mundoasorrir.mundoasorrirbackend.DTO.Project.ProjectMapper;
 import com.mundoasorrir.mundoasorrirbackend.DTO.User.UserMapper;
 import com.mundoasorrir.mundoasorrirbackend.Domain.Project.Project;
 import com.mundoasorrir.mundoasorrirbackend.Domain.User.SystemUser;
+import com.mundoasorrir.mundoasorrirbackend.Message.ErrorMessage;
 import com.mundoasorrir.mundoasorrirbackend.Message.ResponseMessage;
+import com.mundoasorrir.mundoasorrirbackend.Message.SuccessMessage;
 import com.mundoasorrir.mundoasorrirbackend.Services.ProjectService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -36,78 +38,117 @@ public class ProjectController {
     @Autowired
     private final AuthUtils authUtils;
 
+    /**
+     *
+     * @param designation
+     * @param location
+     * @param request
+     * @return
+     */
     @PostMapping("/createProject")
     public ResponseEntity<?> createProject(@RequestParam("designation") String designation, @RequestParam("location")String location, HttpServletRequest request){
         if(!this.authUtils.highPermissions(request)){
-            return ResponseEntity.status(401).body(new MessageResponse("Not allowed"));
+            return ResponseEntity.status(401).body(ErrorMessage.NOT_ALLOWED);
         }
         try{
             this.projectService.save(new Project(designation,location));
-            return ResponseEntity.ok().body(new MessageResponse("Project created successfully!!"));
+            return ResponseEntity.ok().body(SuccessMessage.PROJECT_CREATED);
 
         }catch(Exception e){
-            return ResponseEntity.badRequest().body(new MessageResponse("Error: There has been an error creating the project!!"));
+            return ResponseEntity.badRequest().body(ErrorMessage.ERROR);
         }
     }
+
+    /**
+     *
+     * @return
+     */
     @GetMapping("/getProjects")
     public ResponseEntity<?> getProjects(){
         try{
             return ResponseEntity.ok().body(ProjectMapper.toDTO(this.projectService.getActiveProjects()));
         }catch(Exception e){
-            return ResponseEntity.badRequest().body(new MessageResponse("Error: There has been an error creating the project!!"));
+            return ResponseEntity.badRequest().body(ErrorMessage.ERROR);
         }
     }
+
+    /**
+     *
+     * @param request
+     * @return
+     */
 
     @GetMapping("/getAllProjects")
     public ResponseEntity<?> getAllProjects( HttpServletRequest request){
         if(!this.authUtils.highPermissions(request)){
-            return ResponseEntity.status(401).body(new MessageResponse("Not allowed"));
+            return ResponseEntity.status(401).body(ErrorMessage.NOT_ALLOWED);
         }
 
         try{
             return ResponseEntity.ok().body(ProjectMapper.toDTO(this.projectService.findAll()));
         }catch(Exception e){
-            return ResponseEntity.badRequest().body(new MessageResponse("Error: There has been an error creating the project!!"));
+            return ResponseEntity.badRequest().body(ErrorMessage.ERROR);
         }
     }
+
+    /**
+     *
+     * @param id
+     * @param year
+     * @param month
+     * @param request
+     * @return
+     */
     @GetMapping(value="/getUsersOnVacation/{id}/{year}/{month}")
     public ResponseEntity<?> getUsersOnVacation(@PathVariable Long id, @PathVariable int year, @PathVariable int month , HttpServletRequest request ) {
         if(!this.authUtils.mediumPermissions(request)){
-            return ResponseEntity.status(401).body(new ResponseMessage("Not allowed"));
+            return ResponseEntity.status(401).body(ErrorMessage.NOT_ALLOWED);
         }
         try{
             List<SystemUser> users = this.projectService.getUsersOnVacationInMonth(id,month,year);
             return ResponseEntity.ok().body(UserMapper.toDTO(users));
         }catch(Exception e){
-            return ResponseEntity.badRequest().body(new ResponseMessage("There was an error in getting the users"));
+            return ResponseEntity.badRequest().body(ErrorMessage.ERROR);
         }
     }
 
-
+    /**
+     *
+     * @param id
+     * @param request
+     * @return
+     */
 
     @PostMapping(value="/activateProject/{id}")
     public ResponseEntity<?> activateAccount(@PathVariable Long id, HttpServletRequest request){
         if(!this.authUtils.highPermissions(request)){
-            return ResponseEntity.status(401).body(new MessageResponse("Not allowed"));
+            return ResponseEntity.status(401).body(ErrorMessage.NOT_ALLOWED);
         }
         try{
             this.projectService.activateProject(id);
-            return ResponseEntity.ok(new MessageResponse("Project activated successfully!"));
+            return ResponseEntity.ok(SuccessMessage.PROJECT_ACTIVATED);
         }catch(Exception e){
-            return ResponseEntity.badRequest().body(new ResponseMessage("There was an error in the activation of the project"));
+            return ResponseEntity.badRequest().body(ErrorMessage.ERROR);
         }
     }
+
+    /**
+     *
+     * @param id
+     * @param request
+     * @return
+     */
 
     @PostMapping(value="/deactivateProject/{id}")
     public ResponseEntity<?> deactivateAccount(@PathVariable Long id, HttpServletRequest request){
         if(!this.authUtils.highPermissions(request)){
-            return ResponseEntity.status(401).body(new MessageResponse("Not allowed"));
+            return ResponseEntity.status(401).body(ErrorMessage.NOT_ALLOWED);
         }
         try{
             this.projectService.deactivateProject(id);
-            return ResponseEntity.ok(new MessageResponse("Project deactivated successfully!"));
+            return ResponseEntity.ok(SuccessMessage.PROJECT_DEACTIVATED);
         }catch(Exception e){
-            return ResponseEntity.badRequest().body(new ResponseMessage("There was an error in the activation of the project"));
+            return ResponseEntity.badRequest().body(ErrorMessage.ERROR);
         }
     }
 
